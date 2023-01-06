10ee114598a134a72b1e69493e303cde844950eb
c373 <76661778+c373@users.noreply.github.com>
2023-01-06 09:25:41 -0800

    Adds header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public title = 'CARLOS CRUZ';
+  public title: string = 'First Last';
+  public links: Map<string, string> = new Map([
+    ["github", "https://github.com"],
+    ["linkedin", "https://linkedin.com"],
+    ["projects", "https://projects.projects"]
+  ]);
 }
